a12d614a35cda6737e8a58fee4f0a85ef0d707cb
surasakul1256 <60254124+surasakul1256@users.noreply.github.com>
2020-03-25 15:16:41 +0700

    MOTOR and Encoder

---
+++ Lab10/Ex3/Ex3.ino
@@ -0,0 +1,33 @@
+#define CHA 2
+#define CHB 3
+
+int previousA = 0;
+int A = 0;
+int B = 0;
+
+void setup(){
+  pinMode(CHA, INPUT_PULLUP);
+  pinMode(CHB, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop(){
+  A = digitalRead(CHA);
+  B = digitalRead(CHB);
+  
+  if(previousA == 0 && A == 1)
+  {
+    if(B == 1)
+    {
+      Serial.println("Counter-Clockwise");
+      delay(700);
+    }
+    if(B == 0)
+    {
+      Serial.println("Clockwise");
+      delay(700);
+    }
+  }
+  
+  previousA = A;
+}
